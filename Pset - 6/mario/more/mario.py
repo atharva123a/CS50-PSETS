dae8b7c8dc033a8ecddd5c2a840211b21351f1bb
atharva123a <64686082+atharva123a@users.noreply.github.com>
2021-09-29 22:08:05 +0530

    pset 6 - mario/more

--- Pset - 6/mario/more/mario.py
+++ Pset - 6/mario/more/mario.py
@@ -1,1 +1,40 @@
+from cs50 import get_int
+
+def main():
+    #main function:
+    size = get_positive_int()
+
+    for i in range(size):
+
+        # printing empty spaces:
+        k = size - i - 1
+
+        while k > 0:
+            print(" ", end = "")
+            k -= 1
+
+        # first set of hashes:
+        for hashes  in range(i + 1):
+            print("#", end  = "")
+
+        print("  ", end = "")
+        # next set of hashes:
+        for n_hashes in range(i + 1):
+            print("#", end = "")
+
+        #new line
+        print()
+
+
+def get_positive_int():
+
+    while True:
+        size = get_int("Size: ")
+
+        if size >= 1 and size <= 8:
+            break
+
+    return size
+
+main()
 
